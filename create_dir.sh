7b877495b139161c74f1d160d625fa209ab7c8ff
naufalbasara <naufalbasara@outlook.com>
2024-12-26 08:17:13 +0700

    feat: add mkdir scripts

---
+++ create_dir.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir ./result_data/article_links
+
+mkdir ./result_data/article_links/Kesehatan
+mkdir ./result_data/article_links/Otomotif
+mkdir ./result_data/article_links/Pemilu
+mkdir ./result_data/article_links/Super\ Ball
+mkdir ./result_data/article_links/Techno
+mkdir ./result_data/article_links/Travel
